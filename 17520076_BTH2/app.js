0c7bbf9572a48ee78a9a331a3279b05becfb1284
= <=>
2019-04-03 19:16:19 +0700

    Update April 3rd

---
+++ 17520076_BTH2/app.js
@@ -0,0 +1,25 @@
+﻿const http = require('http');
+
+const fs = require('fs');
+
+const module1 = require('./mymodules/libs');
+
+function onRequest(req,res) {
+    fs.readFile('./home.html', (err, data) => {
+        if (err) {
+            res.writeHead(404);
+            res.write('File not Found');
+        }
+        else {
+            res.writeHead(200, {
+                'Content-type': 'text/html'
+            });
+            res.write(data);
+        };
+        res.end();
+    });
+}
+
+const server = http.createServer(onRequest).listen(module1.port, module1.hostname, () => {
+    module1.show();
+})
---
+++ 17520076_BTH2/mymodules/libs.js
@@ -0,0 +1,7 @@
+﻿module.exports = {
+    show: function () {
+        console.log(`Server running at http://${this.hostname}:${this.port}/`);
+    },
+    hostname:'127.0.0.1',
+    port:3000
+};
